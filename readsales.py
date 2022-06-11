0eedead62034aab92159225159280f38a11ac402
Zach Frey <zach@znfrey.com>
2022-06-10 22:30:28 -0400

    WIP: Got parsing of first item (product name)

--- readsales.py
+++ readsales.py
@@ -1,6 +1,8 @@
 
 # Return List of lines in the sales file
-from re import A
+#from re import A
+import re
+import pyparsing
 
 
 # Read all lines from the sales file
@@ -10,6 +12,15 @@ def readsales(fname):
     file.close()
     return sales
 
-
+# Split into individual sales records by comma
 def splitsales(line):
     return line.split(",")
+
+# Parse a single sales record into a Dictionary
+def parsesale(sale):
+    sale_tokens = sale.split()
+    # TODO: put into Dictionary
+    sale_record = { }
+    search_results = re.finditer(r'\<.*?\>', sale)
+    sale_record["product"] = next(search_results).group().strip('<>')
+    return sale_record
--- test_readsales.py
+++ test_readsales.py
@@ -1,6 +1,18 @@
+from re import L
 import readsales
 
 def test_readsales():
     sales = readsales.readsales("example.csv");
     assert(sales is not None)
     assert(isinstance(sales, list))
+
+
+def test_parsesale():
+    sale = readsales.parsesale("<example product> <997> <GBP> <example@email> <N> <Ben> <Corey>")
+    assert(sale is not None)
+    assert(isinstance(sale, dict))
+    assert(sale["product"] == "example product")
+    assert(sale["price"] is 997)
+    assert(sale["currency"] is "GBP")
+    assert(sale["email"] is "example@email")
+    assert(sale["bonus"] is "N")
