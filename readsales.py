1b31d9714c2711285c8557aedce7a7f96e3bfc89
Zach Frey <zach@znfrey.com>
2022-06-10 22:47:19 -0400

    Got parse of one sales record to work

--- readsales.py
+++ readsales.py
@@ -1,8 +1,7 @@
 
 # Return List of lines in the sales file
-#from re import A
+
 import re
-import pyparsing
 
 
 # Read all lines from the sales file
@@ -22,5 +21,13 @@ def parsesale(sale):
     # TODO: put into Dictionary
     sale_record = { }
     search_results = re.finditer(r'\<.*?\>', sale)
-    sale_record["product"] = next(search_results).group().strip('<>')
+    sale_record["product"]  = next(search_results).group().strip('<>')
+    sale_record["price"]    = int(next(search_results).group().strip('<>'))
+    sale_record["currency"] = next(search_results).group().strip('<>')
+    sale_record["email"]    = next(search_results).group().strip('<>')
+    sale_record["bonus"]    = next(search_results).group().strip('<>')
+    sale_record["seller"]   = []
+    for item in search_results:
+        sale_record["seller"].append(item.group().strip('<>'))
+    
     return sale_record
