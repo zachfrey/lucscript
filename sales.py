2d81dcd0f20d808d013d95af931e8012a63f7f3f
Zach Frey <zach@znfrey.com>
2022-06-27 20:43:49 -0400

    Handle multiline input data. Make output prettier.

--- sales.py
+++ sales.py
@@ -14,11 +14,16 @@ if __name__ == "__main__":
         sys.exit(os.EX_USAGE)
     sales_file = read_sales(sys.argv[1])
 
-    sales_list = parse_sales_list(sales_file[0])
+    sales_list = []
+    for line in sales_file:
+        sales_list += parse_sales_list(line)
 
-    # read currency conversion
+    # Read currency conversion rates (to $USD)
     currencies = read_currency_conversion(sys.argv[2])
 
+    # Do the math to compute everyone's commissions
     commissions = calculate_commissions(sales_list, currencies, bonus_rates)
 
-    print(commissions)
+    # Print out the results
+    for seller in commissions:
+        print(f"{seller}\t${round(commissions[seller],2)}")
